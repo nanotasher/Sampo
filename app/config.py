83d39e45b2165a622d0713c24db1598ee7d9df52
Nathan Rose <nathan.p.rose@gmail.com>
2025-11-14 21:16:58 -0600

    More config changes

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     app_name: str = "Sampo"
-    environment: str = os.getenv("ENVIRONMENT", "https://sampo-0edyka.fly.dev")
+    environment: str = os.getenv("ENVIRONMENT", "local")
     port: int = int(os.getenv("PORT", "8080"))
 
     db_url: str = os.getenv("DB_URL", "sqlite:///./sampo.db")
